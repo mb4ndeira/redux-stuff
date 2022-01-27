9074e68b453b55bededeba482ab8dd0ec916d004
mb4ndeira <mbandeira.cs@gmail.com>
2022-01-27 11:44:57 +0000

    feat: implement products API to catalog component

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import api from '../services/api';
 
-// Import {useSelector} from 'react-redux';
+import {IProduct} from '../store/modules/cart/types';
 
-const Catalog:React.FC = () =>
-// Const catalog = useSelector(state => state);
+const Catalog: React.FC = () => {
+    const [catalog, setCatalog] = useState<IProduct[]>([]);
 
-    (<h1>Catalog</h1>);
+    useEffect(() => {
+        api.get('products').then(response => {
+            setCatalog(response.data);
+        });
+    }, []);
+
+    return (<main>
+        <h1>Catalog</h1>
+        {catalog.map(product => (
+            <article key={product.id}>
+                <strong>{product.title}</strong>
+                <span>{product.price}</span>
+
+                <button type="button">Comprar</button>
+            </article>
+        ))}
+    </main>);
+};
+
 export default Catalog;
