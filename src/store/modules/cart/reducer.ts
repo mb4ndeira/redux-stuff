84c17b58eca112c36447a3653075ebad581d1433
mb4ndeira <mbandeira.cs@gmail.com>
2022-01-27 13:59:50 +0000

    feat: implement addProductToCart action into cart reducer

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -6,6 +6,18 @@ const INITIAL_STATE: Cart = {
     items: [],
 };
 
-const cart: Reducer<Cart> = () => INITIAL_STATE;
+const cart: Reducer<Cart> = (state = INITIAL_STATE, action) => { // eslint-disable-line default-param-last
+    switch (action.type) {
+        case 'ADD_PRODUCT_TO_CART': {
+            const {product} = action.payload;
+
+            return {...state, items: [...state.items, {product, quantity: 1}]};
+        }
+
+        default: {
+            return state;
+        }
+    }
+};
 
 export default cart;
