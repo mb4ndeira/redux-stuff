1eb46f8b59f16ca583d9be672c6d63abd9f18d62
mb4ndeira <mbandeira.cs@gmail.com>
2022-01-27 10:27:42 +0000

    feat: add cart reducer

---
+++ src/store/modules/cart/reducer.ts
@@ -0,0 +1,11 @@
+import {Reducer} from 'redux';
+
+import {Cart} from './types';
+
+const INITIAL_STATE: Cart = {
+	items: [],
+};
+
+const cart: Reducer<Cart> = () => INITIAL_STATE;
+
+export default cart;
---
+++ src/store/modules/cart/types.ts
@@ -0,0 +1,14 @@
+export interface IProduct {
+	id: number;
+	title: string;
+	price: number;
+}
+
+export interface ICartItem {
+	product: IProduct;
+	quantity: number;
+}
+
+export type Cart = {
+	items: ICartItem[]
+}
