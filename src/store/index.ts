14f7aea747fd301c880955f8f0643ae4999593e1
mb4ndeira <mbandeira.cs@gmail.com>
2022-01-27 14:12:59 +0000

    feat: add types to redux store

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,6 +2,12 @@ import {createStore} from 'redux';
 
 import rootReducer from './modules/rootReducer';
 
+import {Cart} from './modules/cart/types';
+
+export interface IState {
+    cart: Cart
+}
+
 const store = createStore(rootReducer);
 
 export default store;
