05502834f726e7546fdc5079a671e592497142ac
mb4ndeira <mbandeira.cs@gmail.com>
2022-01-28 20:38:02 +0000

    feat: divide product addition redux action for correct reaction to stock checking middleware

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -10,7 +10,7 @@ const INITIAL_STATE: Cart = {
 const cart: Reducer<Cart> = (state = INITIAL_STATE, action) => // eslint-disable-line default-param-last
     produce(state, draft => {
         switch (action.type) {
-            case 'ADD_PRODUCT_TO_CART': {
+            case 'ADD_PRODUCT_TO_CART_SUCCESS': {
                 const {product} = action.payload;
 
                 const productCartIndex = draft.items.findIndex(item => item.product.id === product.id);
@@ -21,6 +21,10 @@ const cart: Reducer<Cart> = (state = INITIAL_STATE, action) => // eslint-disable
                     draft.items.push({product, quantity: 1});
                 }
 
+                break;
+            }
+
+            case 'ADD_PRODUCT_TO_CART_FAILURE': {
                 break;
             }
 
